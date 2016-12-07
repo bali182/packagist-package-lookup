ea2f1a55b508d16700b46603283700493e755f8f
Balu <edesbalazs@gmail.com>
2016-12-07 20:11:45 +0000

    update node-fetch to axios

---
+++ index.test.js
@@ -0,0 +1,37 @@
+var packagist = require('./index')
+
+describe('packagist-package-lookup tests', function () {
+
+  it('should find packages by name', function () {
+    return packagist.searchByName('monolog').then(function (result) {
+      expect(result).toBeInstanceOf(Array)
+      expect(result.length).toBeGreaterThan(0)
+      result.forEach(function (element) {
+        expect(typeof element.name).toBe('string')
+        expect(typeof element.description).toBe('string')
+        expect(typeof element.url).toBe('string')
+        expect(typeof element.repository).toBe('string')
+      })
+    })
+  })
+
+  it('should find packages by vendor', function () {
+    return packagist.searchByVendor('monolog').then(function (result) {
+      expect(result).toBeInstanceOf(Array)
+      expect(result.length).toBeGreaterThan(0)
+      result.forEach(function (element) {
+        expect(typeof element).toBe('string')
+      })
+    })
+  })
+
+  it('should list package versions', function () {
+    return packagist.versions('monolog/monolog').then(function (result) {
+      expect(result).toBeInstanceOf(Array)
+      expect(result.length).toBeGreaterThan(0)
+      result.forEach(function (element) {
+        expect(typeof element).toBe('string')
+      })
+    })
+  })
+})
